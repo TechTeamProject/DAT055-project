25d146b749b3fec2d280c3d4d5030ffe9b6456e6
davidbehdadpoor <98192487+davidbehd@users.noreply.github.com>
2022-02-17 21:52:11 +0100

    Lagt till en box till veckonamn och en annan till datum

--- src/View/WeekView.java
+++ src/View/WeekView.java
@@ -7,22 +7,36 @@ import java.awt.*;
 public class WeekView extends TopBar {
 
     private JPanel contentPane;
+    private String [] weekDays =  new String[]{"MON" , "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
 
 
 
     public WeekView(){
 
-        //ContentPane
-
         contentPane = new JPanel(new GridLayout(1,7));
         add(contentPane);
 
-        for(int i=0; i<7; ++i){
+        for(int i=0; i<weekDays.length; ++i){
+
+            //seven vertical boxes representing each day of the week
             JPanel dayBox = new JPanel(new GridLayout(8,1));
             dayBox.setBorder(new EtchedBorder());
-            JPanel titleBox = new JPanel();
-            dayBox.add(titleBox);
+
+            //Each dayBox has a titleBox consisting of two vertical boxes
+            JPanel titleBox = new JPanel(new GridLayout(2,1));
             titleBox.setBorder(new EtchedBorder());
+            dayBox.add(titleBox);
+
+            //upper half of the titleBox
+            JPanel dateBox = new JPanel();
+            titleBox.add(dateBox);
+            dateBox.add(new JLabel(weekDays[i]));
+
+            //lower half of the titleBox
+            JPanel titelBoxDown = new JPanel();
+            titelBoxDown.add(new JLabel("?"));
+            titleBox.add(titelBoxDown);
+
             contentPane.add(dayBox);
         }
 

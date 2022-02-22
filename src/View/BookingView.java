0d4371aad0a92d8f58b00b53044a53b374fea3e3
eliasc99 <eliascar@student.chalmers.se>
2022-02-22 19:53:05 +0100

    bookingview

---
+++ src/View/BookingView.java
@@ -0,0 +1,42 @@
+package src.View;
+import src.Event;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+public class BookingView extends JPanel {
+    public BookingView(LinkedList<Event> list){
+        update(list);
+    }
+    private void update(LinkedList<Event> list){
+        this.removeAll();
+        this.setLayout(new GridLayout(list.size()+1, 1, 5, 5)); //Sätter antal rader till antal events + titel
+        JLabel header = new JLabel("Bookings");
+        this.add(header);
+        for(Event e : list){//Lägger till en panel för varje event med
+            JPanel p = new JPanel(new GridLayout(2,2,10,10));
+            //String title_ = e.getTitle();
+            //JLabel title = new JLabel(title_);
+            //String time_ = e.getTime();
+            //JLabel time = new JLabel(time_);
+            //String location_ = e.getLocation();
+            //JLabel location = new JLabel(location_);
+            JButton edit = new JButton("EDIT");
+            //p.add(title);
+            //p.add(time);
+            //p.add(location);
+            p.add(edit);
+            this.add(p);
+        }
+    }
+    private class Listener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //Edit knapp
+
+        }
+    }
+}

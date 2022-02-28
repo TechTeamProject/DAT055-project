1a3796a886930c4351bb26d6cbb45c9606674a03
eliasc99 <eliascar@student.chalmers.se>
2022-02-28 18:03:31 +0100

    gå från yearview till monthview, ej klar

--- src/View/YearView.java
+++ src/View/YearView.java
@@ -10,7 +10,7 @@ import javax.swing.border.EmptyBorder;
 import java.time.Year;
 
 
-public class YearView extends JPanel{
+public class YearView extends JPanel implements ActionListener{
 
     private int currentYear;
     private int previousYear;
@@ -20,22 +20,42 @@ public class YearView extends JPanel{
 
         JPanel p = new JPanel(new GridLayout(4,4,20,20));
         JButton button1 = new JButton("January");
+        button1.addActionListener(this);
+        button1.setActionCommand("1");
         JButton button2 = new JButton("February");
+        button2.addActionListener(this);
+        button2.setActionCommand("2");
         JButton button3 = new JButton("March");
+        button3.addActionListener(this);
+        button3.setActionCommand("3");
         JButton button4 = new JButton("April");
+        button4.addActionListener(this);
+        button4.setActionCommand("4");
         JButton button5 = new JButton("May");
+        button5.addActionListener(this);
+        button5.setActionCommand("5");
         JButton button6 = new JButton("June");
+        button6.addActionListener(this);
+        button6.setActionCommand("6");
         JButton button7 = new JButton("July");
+        button7.addActionListener(this);
+        button7.setActionCommand("7");
         JButton button8 = new JButton("August");
+        button8.addActionListener(this);
+        button8.setActionCommand("8");
         JButton button9 = new JButton("September");
+        button9.addActionListener(this);
+        button9.setActionCommand("9");
         JButton button10 = new JButton("October");
+        button10.addActionListener(this);
+        button10.setActionCommand("10");
         JButton button11 = new JButton("November");
+        button11.addActionListener(this);
+        button11.setActionCommand("11");
         JButton button12 = new JButton("December");
+        button12.addActionListener(this);
+        button12.setActionCommand("12");
         setPreferredSize( new Dimension( 600, 400 ) );
-
-
-
-
 
         JButton previous = new JButton("<");
         JButton next = new JButton(">");
@@ -82,6 +102,13 @@ public class YearView extends JPanel{
                 }
             }
         });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
+        int month = Integer.parseInt(cmd);
+        //visa monthView med datum month/currentYear
     }
 }
 

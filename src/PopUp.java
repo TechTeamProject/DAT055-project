24a5091c951d0326fba8fc9f38f4db64bc25458c
Erik Gustavsson <egust@student.chalmers.se>
2022-03-06 20:11:41 +0100

    Javadoc popup

--- src/PopUp.java
+++ src/PopUp.java
@@ -3,14 +3,21 @@ package src;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 
-/**
+/**@author Erik Gustavsson
+ * @version 1.0
+ * @since 2022-03-06
+ *
  * Class used to create a small rightclickpopup with options.
+ * Using JPopupMenu superclass
  */
 public class PopUp extends JPopupMenu{
     private JMenuItem popup;
     private JMenuItem popupRemove;
-    /**
+
+    /**Constructor
+     *
      * @param s, s is a string name to determine which view we are rightclicking in.
+     *           Possibility to enable rightclick in more panels if needed.
      */
     public PopUp(String s) {
         if (s.equals("week")) {
@@ -18,7 +25,6 @@ public class PopUp extends JPopupMenu{
             popupRemove = new JMenuItem("Remove Event");
             add(popup);
             add(popupRemove);
-
         }
     }
 
